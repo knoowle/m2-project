9ffa0d94b2ec1ff3138cbd36d0d76df1d61c87ae
john.green <shunzheheliu@163.com>
2013-01-24 17:29:34 +0800

    ss

---
+++ hibernate-example/src/main/java/com/knoowle/example/servlet/IndexServlet.java
@@ -0,0 +1,19 @@
+package com.knoowle.example.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class IndexServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 7418896333426150414L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/pages/index.jsp").forward(request, response);
+	}
+}
